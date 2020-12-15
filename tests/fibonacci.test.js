0531b2a1714c0804e7e192a777118e0ecfe8dc85
Gabriel Carvalho <bielcarvalho7@gmail.com>
2020-12-14 21:27:30 -0300

    Atualização final

--- tests/fibonacci.test.js
+++ tests/fibonacci.test.js
@@ -1,11 +1,11 @@
 const calc = require('../fibonacci')
-const resposta = "[0, 1, 1, 2, 3]"
-const resposta2 = "[0, 1, 1, 2, 3]"
+const resposta = [0, 1, 1, 2, 3]
+const resposta2 = [0,1,1,2,3,5]
 
 test('fibonacci', ()=>{
-    expect(calc.fibonacci(5)).toBe(resposta)
+    expect(calc.fibonacci(5)).toStrictEqual(resposta)
 })
 
 test('fibonacci', ()=>{
-    expect(calc.fibonacci(6)).toBe(resposta2)
+  expect(calc.fibonacci(6)).toStrictEqual(resposta2)
 })
--- tests/quicksort.test.js
+++ tests/quicksort.test.js
@@ -1,10 +1,13 @@
 const calc = require('../quicksort')
-
+const array1 = [5,3,2,1]
+const array2 = [8,7,12,10]
+const resposta1 = [1,2,3,5]
+const resposta2 = [7,8,10,12]
 
 test('Ordernar um array', ()=>{
-    expect(calc.quickSort([5,3,2,1])).toBe("[1,2,3,5]")
+    expect(calc.quickSort(array1)).toStrictEqual(resposta1)
 })
 
 test('Ordernar um array', ()=>{
-    expect(calc.quickSort([8,7,12,10])).toBe("[7,8,10,12]")
+    expect(calc.quickSort(array2)).toStrictEqual(resposta2)
 })
